1eebdbf98bc2eab8dfd13511477398ef5c172893
David Phillips <david@daviddouglasphillips.com>
2015-11-01 13:14:14 -0800

    Added slack dep

--- mix.exs
+++ mix.exs
@@ -27,6 +27,7 @@ defmodule Georgi.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:slack, "~> 0.3.0"},
+     {:websocket_client, git: "https://github.com/jeremyong/websocket_client"}]
   end
 end
