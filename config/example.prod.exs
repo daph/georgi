15a551eb54114648420af555e18e5a10193558c8
David Phillips <david@daviddouglasphillips.com>
2015-11-03 21:38:24 -0800

    Fixing config

--- config/example.prod.exs
+++ config/example.prod.exs
@@ -1,8 +1,9 @@
-config :georgi
+use Mix.Config
+config :georgi,
 
 # Slack tokens must be a list
 # This allows one app to connect to multiple slack chats
-slack_tokens: ["token1"]
+  slack_tokens: ["token1", "token2"],
 
 # The input text file
-corpus: ["example.txt"]
+  corpus: "example.txt"
